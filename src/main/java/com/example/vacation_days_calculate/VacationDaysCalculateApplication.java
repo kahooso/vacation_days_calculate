572d4b2cc52414436cbb56e5a7a74451c7658687
kahooso <nikitapobircev@gmail.com>
2024-03-27 15:31:27 +0300

    new project, neoflex first task!

---
+++ src/main/java/com/example/vacation_days_calculate/VacationDaysCalculateApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacation_days_calculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationDaysCalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationDaysCalculateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vacation_days_calculate/VacationDaysCalculateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacation_days_calculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationDaysCalculateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
